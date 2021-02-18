9e6c48ac6f552f33e27e9435a73605cfb26917e8
Eder Benjamin <ben-eder@freenet.de>
2021-02-18 20:06:25 +0100

    Changed seasons css class names

--- src/pages/Seasons.vue
+++ src/pages/Seasons.vue
@@ -1,10 +1,10 @@
 <template>
   <section class="presence-container--flex">
     <MdcCard v-for="season in seasons" :key="season.id"
-             class="presence-season__card" :class="{'presence-season__card--active': season.isCurrentSeason}">
+             class="presence-seasons__card" :class="{'presence-seasons__card--active': season.isCurrentSeason}">
       <h2>{{ season.name }}</h2>
       <hr>
-      <nav class="presence-season__card-actions">
+      <nav class="presence-seasons__card-actions">
         <MdcButton title="Edit" mode="outlined" />
         <MdcButton title="Open" />
       </nav>
@@ -37,17 +37,17 @@ export default defineComponent({
 @use "../assets/style/presence-container";
 @use "../assets/style/presence-color";
 
-.presence-season__card {
+.presence-seasons__card {
   width: 25rem;
   max-width: 80%;
   text-align: center;
 }
 
-.presence-season__card--active {
+.presence-seasons__card--active {
   border: 2px solid presence-color.$primary;
 }
 
-.presence-season__card-actions {
+.presence-seasons__card-actions {
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
