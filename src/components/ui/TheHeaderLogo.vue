e91e6bcda7c04955b3a77cb16d99ae75b86b3d03
Eder Benjamin <ben-eder@freenet.de>
2021-02-18 09:20:38 +0100

    Added the app logo to the header

---
+++ src/components/ui/TheHeaderLogo.vue
@@ -0,0 +1,14 @@
+<template>
+  <router-link :to="{name: 'home'}" class="presence-header__logo">
+    <img src="../../assets/logo.png" alt="Blue Army Logo">
+  </router-link>
+</template>
+
+<style scoped>
+.presence-header__logo,
+.presence-header__logo > img {
+  border-radius: 100%;
+  width: 3rem;
+  height: 3rem;
+}
+</style>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,1 +1,2 @@
 export { default as TheHeader } from './TheHeader.vue';
+export { default as TheHeaderLogo } from './TheHeaderLogo.vue';
