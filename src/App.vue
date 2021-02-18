6f252c2acd7e09451395244859d03d0258bc29b7
Eder Benjamin <ben-eder@freenet.de>
2021-02-18 10:56:32 +0100

    Added slide transition to router view

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <TheHeader />
   <main>
-    <router-view />
+    <router-view v-slot="{Component}">
+      <transition name="page-slide" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </main>
   <TheFooter />
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 import { TheFooter, TheHeader } from './components';
 
-export default {
+export default defineComponent({
   components: {
     TheHeader,
     TheFooter
   }
-}
+});
 </script>
 
 <style>
@@ -35,6 +40,37 @@ body {
 
 #app > main {
   flex-grow: 1;
+  overflow-x: hidden;
   overflow-y: auto;
+}
+
+.page-slide-enter-active {
+  animation: slide-in 0.3s ease-out;
+}
+
+.page-slide-leave-active {
+  animation: slide-out 0.3s ease-in;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-50px);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+  to {
+    transform: translateX(50px);
+    opacity: 0;
+  }
 }
 </style>
