57e9d82ded7f69f62a7571d2ebf41351f940091b
Eder Benjamin <ben-eder@freenet.de>
2021-02-18 19:32:50 +0100

    Implemented seasons page

--- src/pages/Seasons.vue
+++ src/pages/Seasons.vue
@@ -1,23 +1,55 @@
 <template>
-  <MdcCard>
-    <section class="presence-container--flex">
-      <h1>Seasons</h1>
-    </section>
-  </MdcCard>
+  <section class="presence-container--flex">
+    <MdcCard v-for="season in seasons" :key="season.id"
+             class="presence-season__card" :class="{'presence-season__card--active': season.isCurrentSeason}">
+      <h2>{{ season.name }}</h2>
+      <hr>
+      <nav class="presence-season__card-actions">
+        <MdcButton title="Edit" mode="outlined" />
+        <MdcButton title="Open" />
+      </nav>
+    </MdcCard>
+  </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { MdcCard } from "../components";
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { MdcButton, MdcCard } from '../components';
+import { Season } from '../store/seasons';
 
 export default defineComponent({
   components: {
-    MdcCard
+    MdcCard,
+    MdcButton
+  },
+  setup() {
+    const seasons = computed(() => useStore().getters['seasons/getSeasons'] as Season[]);
+
+    return {
+      seasons
+    };
   }
 });
 </script>
 
-
 <style lang="scss" scoped>
 @use "../assets/style/presence-container";
+@use "../assets/style/presence-color";
+
+.presence-season__card {
+  width: 25rem;
+  max-width: 80%;
+  text-align: center;
+}
+
+.presence-season__card--active {
+  border: 2px solid presence-color.$primary;
+}
+
+.presence-season__card-actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
 </style>
