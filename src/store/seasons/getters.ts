e39d7187046f6a3522e6e7626dec75a45b787095
Eder Benjamin <ben-eder@freenet.de>
2021-02-18 19:39:06 +0100

    Added created attribute to season to sort them

--- src/store/seasons/getters.ts
+++ src/store/seasons/getters.ts
@@ -2,5 +2,8 @@ import { GetterTree } from 'vuex';
 import { SeasonsModule } from './index';
 
 export default {
-  getSeasons: state => state.seasons
+  getSeasons: state => {
+    return state.seasons
+      .sort((s1, s2) => s2.created.getTime() - s1.created.getTime());
+  }
 } as GetterTree<SeasonsModule, SeasonsModule>;
--- src/store/seasons/index.ts
+++ src/store/seasons/index.ts
@@ -5,6 +5,7 @@ export interface Season {
   id: string;
   name: string;
   isCurrentSeason: boolean;
+  created: Date;
 }
 
 export interface SeasonsModule {
@@ -18,12 +19,14 @@ export const seasons = {
       {
         id: '1920',
         name: 'DEL Saison 2019/20',
-        isCurrentSeason: false
+        isCurrentSeason: false,
+        created: new Date('2019-07-01')
       },
       {
         id: '2021',
         name: 'Penny DEL Saison 2020/21',
-        isCurrentSeason: true
+        isCurrentSeason: true,
+        created: new Date('2020-07-01')
       }
     ]
   },
