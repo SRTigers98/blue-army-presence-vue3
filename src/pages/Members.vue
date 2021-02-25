54264f88f91f98cfb773e9a6ef1f0dec31b4d854
Eder Benjamin <ben-eder@freenet.de>
2021-02-25 19:05:37 +0100

    feat: created card to display members

--- src/pages/Members.vue
+++ src/pages/Members.vue
@@ -1,13 +1,49 @@
 <template>
-  <h1>Members</h1>
+  <section class="presence-container--flex">
+    <MdcCard class="presence-members__card">
+      <h2>
+        <MdcIcon icon-name="person" class="presence-members__card-icon" />
+        Max Mustermann
+      </h2>
+      <menu class="presence-members__card-actions">
+        <MdcButton mode="danger">Delete</MdcButton>
+        <MdcButton>Edit</MdcButton>
+      </menu>
+    </MdcCard>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { MdcButton, MdcCard, MdcIcon } from '../components';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    MdcButton,
+    MdcCard,
+    MdcIcon
+  }
+});
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@use "../assets/style/presence-container";
 
+.presence-members__card {
+  width: 25rem;
+  max-width: 80%;
+}
+
+.presence-members__card-icon {
+  border-radius: 50%;
+  background-color: darkgray;
+  padding: 0.5rem;
+}
+
+.presence-members__card-actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 0;
+}
 </style>
