6271a11897bcb1425cec2526a0bcedd11d0a62b2
Eder Benjamin <ben-eder@freenet.de>
2021-02-26 19:47:32 +0100

    feat: implemented season games page

--- src/pages/seasons/SeasonGames.vue
+++ src/pages/seasons/SeasonGames.vue
@@ -1,15 +1,84 @@
 <template>
   <section class="presence-container--flex">
-    <h1>Season Games</h1>
+    <MdcCard v-for="game in seasonGames" :key="game.id" class="presence-games__card">
+      <h2>{{ game.home ? 'vs.' : '@' }} {{ game.opponent }}</h2>
+      <section class="presence-games__game-info">
+        <MdcIcon :icon-name="getGameModeIcon(game)" />
+        <span>{{ game.date.toLocaleDateString() }}</span>
+      </section>
+      <hr>
+      <menu class="presence-games__card-menu">
+        <MdcButton mode="danger">
+          <MdcIcon icon-name="delete" />
+        </MdcButton>
+        <MdcButton>Edit</MdcButton>
+      </menu>
+    </MdcCard>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
+import { useStore } from 'vuex';
+import { useRoute } from 'vue-router';
+import { MdcButton, MdcCard, MdcIcon } from '../../components';
+import { Season, SeasonGame } from '../../types';
 
-export default defineComponent({});
+export default defineComponent({
+  components: {
+    MdcCard,
+    MdcIcon,
+    MdcButton
+  },
+  setup() {
+    const store = useStore();
+    const route = useRoute();
+
+    const seasonGames = computed(() => {
+      const seasons = store.getters['season/seasons'] as Season[];
+      return (seasons.find(s => s.id === route.params.seasonId)?.games || [])
+          .sort((g1, g2) => g2.date.getTime() - g1.date.getTime());
+    });
+
+    const getGameModeIcon = (game: SeasonGame): string => {
+      switch (game.mode) {
+        case 'regular':
+          return 'event';
+        case 'playoffs':
+          return 'whatshot';
+      }
+    };
+
+    return {
+      seasonGames,
+      getGameModeIcon
+    };
+  }
+});
 </script>
 
 <style lang="scss" scoped>
 @use "../../assets/style/presence-container";
+
+.presence-games__card {
+  width: 80%;
+  max-width: 25rem;
+  text-align: center;
+}
+
+.presence-games__game-info {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+}
+
+.presence-games__card-menu {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 0;
+  margin: 0.5rem 0;
+}
 </style>
