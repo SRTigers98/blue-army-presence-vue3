7de854e71b8eb7a49fa34acac3038fa2798df1ff
Eder Benjamin <ben-eder@freenet.de>
2021-02-26 11:15:21 +0100

    refactor: created edit form container

---
+++ src/components/forms/EditFormContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <form class="presence-container--flex" @submit.prevent="onSubmit">
+    <slot></slot>
+    <section class="presence-form__item presence-form__actions">
+      <MdcButton mode="outlined" type="button" @click="goBack">Back</MdcButton>
+      <MdcButton>Save</MdcButton>
+    </section>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { MdcButton } from '../material';
+
+export default defineComponent({
+  components: {
+    MdcButton
+  },
+  props: {
+    onSubmit: {
+      type: Function,
+      required: true
+    }
+  },
+  setup() {
+    const router = useRouter();
+
+    const goBack = () => router.back();
+
+    return {
+      goBack
+    };
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@use "../../assets/style/presence-container";
+@use "../../assets/style/presence-form";
+</style>
--- src/components/forms/SeasonEditForm.vue
+++ src/components/forms/SeasonEditForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <form class="presence-container--flex" @submit.prevent="submitForm">
-    <section>
+  <EditFormContainer :on-submit="submitForm">
+    <section class="presence-form__item">
       <label class="mdc-text-field mdc-text-field--filled" ref="seasonNameTextField">
         <span class="mdc-text-field__ripple"></span>
         <span class="mdc-floating-label" id="season-name">Season Name</span>
@@ -9,7 +9,7 @@
         <span class="mdc-line-ripple"></span>
       </label>
     </section>
-    <section class="presence-form__switch">
+    <section class="presence-form__item presence-form__switch">
       <div class="mdc-switch" ref="isCurrentSeasonSwitch">
         <div class="mdc-switch__track"></div>
         <div class="mdc-switch__thumb-underlay">
@@ -20,23 +20,18 @@
       </div>
       <label for="current-season">Is Current Season</label>
     </section>
-    <section class="presence-form__actions">
-      <MdcButton mode="outlined" type="button" @click="goBack">Back</MdcButton>
-      <MdcButton>Save</MdcButton>
-    </section>
-  </form>
+  </EditFormContainer>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { useRouter } from 'vue-router';
 import { MDCTextField } from '@material/textfield/component';
 import { MDCSwitch } from '@material/switch/component';
-import { MdcButton } from '../material';
+import EditFormContainer from './EditFormContainer.vue';
 
 export default defineComponent({
   components: {
-    MdcButton
+    EditFormContainer
   },
   props: {
     name: {
@@ -49,8 +44,6 @@ export default defineComponent({
     }
   },
   setup(props, context) {
-    const router = useRouter();
-
     const seasonName = ref<string>(props.name);
     const isCurrentSeason = ref<boolean>(props.isCurrent);
 
@@ -58,13 +51,11 @@ export default defineComponent({
         'season-edit',
         { seasonName: seasonName.value, isCurrentSeason: isCurrentSeason.value }
     );
-    const goBack = () => router.back();
 
     return {
       seasonName,
       isCurrentSeason,
-      submitForm,
-      goBack
+      submitForm
     };
   },
   mounted() {
@@ -75,24 +66,5 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-@use "../../assets/style/presence-container";
-
-section {
-  width: 80%;
-  max-width: 350px;
-  margin: 0.75rem 0;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-}
-
-.presence-form__switch > label {
-  margin-left: 1rem;
-}
-
-.presence-form__actions {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-}
+@use "../../assets/style/presence-form";
 </style>
