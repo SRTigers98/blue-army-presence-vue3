283e1b996c99e7851eccda605d62ff227ba848dc
Eder Benjamin <ben-eder@freenet.de>
2021-02-27 16:21:20 +0100

    feat: added date to game form

--- src/components/forms/SeasonGameEditForm.vue
+++ src/components/forms/SeasonGameEditForm.vue
@@ -6,20 +6,24 @@
     <section class="presence-form__item">
       <MdcSwitch v-model="home" :label="home ? 'Home' : 'Away'" />
     </section>
+    <section class="presence-form__item">
+      <MdcDateField v-model="date" label="Date" required />
+    </section>
   </EditFormContainer>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType, ref } from 'vue';
 import { EditFormContainer } from './util';
-import { MdcSwitch, MdcTextField } from '../material';
+import { MdcDateField, MdcSwitch, MdcTextField } from '../material';
 import { SeasonGame } from '../../types';
 
 export default defineComponent({
   components: {
     EditFormContainer,
     MdcTextField,
-    MdcSwitch
+    MdcSwitch,
+    MdcDateField
   },
   props: {
     gameData: {
@@ -30,15 +34,18 @@ export default defineComponent({
   setup(props, context) {
     const opponent = ref<string>(props.gameData?.opponent || '');
     const home = ref<boolean>(props.gameData ? props.gameData.home : true);
+    const date = ref<Date>(props.gameData?.date || new Date());
 
     const submitForm = () => context.emit('game-edit', {
       opponent: opponent.value,
-      home: home.value
+      home: home.value,
+      date: date.value
     } as SeasonGame);
 
     return {
       opponent,
       home,
+      date,
       submitForm
     };
   }
