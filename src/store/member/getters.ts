84203cd0000493648a458ee803eb961f3fe20d0b
Eder Benjamin <ben-eder@freenet.de>
2021-02-28 10:59:54 +0100

    feat: added store getter to get only active members

--- src/store/member/getters.ts
+++ src/store/member/getters.ts
@@ -1,9 +1,13 @@
 import { GetterTree } from 'vuex';
-import { MemberModule } from '../../types';
+import { Member, MemberModule } from '../../types';
 
 export default {
   members: state => {
     return state.members
       .sort((m1, m2) => (m1.lastName > m2.lastName) ? 1 : ((m2.lastName > m1.lastName) ? -1 : 0));
+  },
+  activeMembers: (state, getters) => {
+    const members = getters.members as Member[];
+    return members.filter(m => m.active);
   }
 } as GetterTree<MemberModule, MemberModule>;
