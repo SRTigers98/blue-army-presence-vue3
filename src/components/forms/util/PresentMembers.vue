9482061f0666617c4823845c138cf8ee7061fbb9
Eder Benjamin <ben-eder@freenet.de>
2021-02-28 11:40:56 +0100

    feat: toggle chips to select present members

--- src/components/forms/util/PresentMembers.vue
+++ src/components/forms/util/PresentMembers.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="mdc-chip-set presence-members__container" role="grid" ref="memberSet">
-    <div v-for="member in availableMembers" :key="member.id" class="mdc-chip" role="row">
+    <div v-for="member in availableMembers" :key="member.id" role="row"
+         class="mdc-chip" :class="{'active': containsMember(member.id)}">
       <div class="mdc-chip__ripple"></div>
       <span role="gridcell">
-      <span role="button" tabindex="0" class="mdc-chip__primary-action">
+      <span role="button" tabindex="0" class="mdc-chip__primary-action" @click="toggleMember(member.id)">
         <span class="mdc-chip__text">{{ member.lastName }}, {{ member.firstName }}</span>
       </span>
     </span>
@@ -12,19 +13,39 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue';
+import { computed, defineComponent, PropType } from 'vue';
 import { MDCChipSet } from '@material/chips/chip-set/component';
 import { Member } from '../../../types';
 
 export default defineComponent({
   props: {
     modelValue: {
-      type: Array
+      type: Array as PropType<string[]>
     },
     availableMembers: {
       type: Array as PropType<Member[]>,
       required: true
     }
+  },
+  setup(props, context) {
+    const presentMembers = computed<string[]>(() => props.modelValue || []);
+
+    const containsMember = (memberId: string): boolean => presentMembers.value.includes(memberId);
+    const toggleMember = (memberId: string) => {
+      const present = [...presentMembers.value];
+      const entryIdx = present.findIndex(id => id === memberId);
+      if (~entryIdx) {
+        present.splice(entryIdx, 1);
+      } else {
+        present.push(memberId);
+      }
+      context.emit('update:modelValue', present);
+    };
+
+    return {
+      containsMember,
+      toggleMember
+    };
   },
   mounted() {
     new MDCChipSet(this.$refs.memberSet as Element);
@@ -33,11 +54,22 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@use "../../../assets/style/presence-color";
+
 .presence-members__container {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   justify-content: space-evenly;
   padding: 0;
+}
+
+.mdc-chip {
+  transition: all 0.5s ease-out;
+}
+
+.mdc-chip.active {
+  background-color: presence-color.$primary;
+  color: presence-color.$on-primary;
 }
 </style>
