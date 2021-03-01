5f98cf775733a47ba4148ff09e5402d8335e6381
Eder Benjamin <ben-eder@freenet.de>
2021-03-01 12:34:30 +0100

    feat: created login form

---
+++ src/components/forms/LoginForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form class="presence-container--flex presence-login__form" @submit.prevent="onLogin">
+    <section class="presence-form__item">
+      <MdcTextField v-model="email" label="E-Mail" required />
+    </section>
+    <section class="presence-form__item">
+      <MdcTextField v-model="password" label="Password" password required />
+    </section>
+    <section class="presence-form__item">
+      <MdcButton>Login</MdcButton>
+    </section>
+  </form>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { MdcButton, MdcTextField } from '../material';
+
+export default defineComponent({
+  components: {
+    MdcTextField,
+    MdcButton
+  },
+  setup(_, context) {
+    const email = ref<string>('');
+    const password = ref<string>('');
+
+    const onLogin = () => context.emit('login', {
+      email: email.value,
+      password: password.value
+    });
+
+    return {
+      email,
+      password,
+      onLogin
+    }
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+@use "../../assets/style/presence-container";
+@use "../../assets/style/presence-form";
+
+.presence-login__form {
+  width: 100%;
+}
+</style>
--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -1,3 +1,4 @@
 export { default as SeasonEditForm } from './SeasonEditForm.vue';
 export { default as MemberEditForm } from './MemberEditForm.vue';
 export { default as SeasonGameEditForm } from './SeasonGameEditForm.vue';
+export { default as LoginForm } from './LoginForm.vue';
