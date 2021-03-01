72dc1903b8cef8f91ae3ca54239aee71ac56939b
Eder Benjamin <ben-eder@freenet.de>
2021-03-01 13:10:10 +0100

    feat: implemented logout

--- src/components/ui/TheHeader.vue
+++ src/components/ui/TheHeader.vue
@@ -5,17 +5,37 @@
         <TheHeaderLogo />
         <span class="mdc-top-app-bar__title">Blue Army Presence</span>
       </section>
+      <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+        <MdcIcon icon-name="logout" class="presence-header__logout" @click="logout" />
+      </section>
     </div>
   </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
+import { useFirebase } from '../../firebase';
 import TheHeaderLogo from './TheHeaderLogo.vue';
+import { MdcIcon } from '../material';
 
 export default defineComponent({
   components: {
-    TheHeaderLogo
+    TheHeaderLogo,
+    MdcIcon
+  },
+  setup() {
+    const router = useRouter();
+    const firebase = useFirebase();
+
+    const logout = () => {
+      firebase.auth().signOut()
+          .then(() => router.replace({ name: 'login' }));
+    };
+
+    return {
+      logout
+    };
   }
 });
 </script>
@@ -23,5 +43,10 @@ export default defineComponent({
 <style lang="scss" scoped>
 .presence-header {
   position: sticky;
+}
+
+.presence-header__logout {
+  margin: 0 1rem;
+  cursor: pointer;
 }
 </style>
