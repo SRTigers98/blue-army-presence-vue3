7555018beec98f7182d5f225d0893578cab63877
Eder Benjamin <ben-eder@freenet.de>
2021-03-01 21:24:54 +0100

    feat: save season to firebase

--- src/store/season/actions/saveSeason.ts
+++ src/store/season/actions/saveSeason.ts
@@ -1,29 +1,19 @@
 import { ActionContext } from 'vuex';
-import { SaveSeasonPayload, Season, SeasonModule } from '../../../types';
+import { useFirebase } from '../../../firebase';
+import { SaveSeasonPayload, SeasonModule } from '../../../types';
 
-export default function (context: ActionContext<SeasonModule, SeasonModule>, payload: SaveSeasonPayload) {
-  const seasons = [...context.getters.seasons as Season[]];
+export default async function (context: ActionContext<SeasonModule, SeasonModule>, payload: SaveSeasonPayload) {
+  const firebaseInstance = useFirebase();
 
-  const seasonIndex = payload.id ? seasons.findIndex(s => s.id === payload.id) : -1;
-  const seasonData: Season = {
-    id: payload.id || (Math.random() * 10000).toFixed(0),
-    name: payload.name,
-    created: payload.created || new Date(),
-    games: []
-  };
+  const seasonRef = payload.id ?
+    firebaseInstance.database().ref(`season/${payload.id}`) :
+    firebaseInstance.database().ref('season').push();
 
-  if (~seasonIndex) {
-    seasonData.games = seasons[seasonIndex].games;
-    seasons[seasonIndex] = seasonData;
-  } else {
-    seasons.push(seasonData);
-  }
-
-  context.commit('seasons', seasons);
+  await seasonRef.child('name').set(payload.name);
+  await seasonRef.child('created').set((payload.created || new Date()).toISOString());
 
   if (payload.isCurrentSeason) {
-    context.commit('currentSeasonId', seasonData.id);
-  } else if (context.getters.currentSeasonId === seasonData.id) {
-    context.commit('currentSeasonId', null);
+    await firebaseInstance.database().ref('currentSeasonId')
+      .set(payload.id);
   }
 }
