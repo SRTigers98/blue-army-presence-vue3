ebb996a3e12a6a8b5334d34713afa184dca4b233
Eder Benjamin <ben-eder@freenet.de>
2021-03-01 21:27:55 +0100

    feat: delete season at firebase

--- src/store/season/actions/deleteSeason.ts
+++ src/store/season/actions/deleteSeason.ts
@@ -1,11 +1,11 @@
 import { ActionContext } from 'vuex';
-import { Season, SeasonModule } from '../../../types';
+import { useFirebase } from '../../../firebase';
+import { SeasonModule } from '../../../types';
 
-export default function (context: ActionContext<SeasonModule, SeasonModule>, seasonId: string) {
-  const seasons = [...context.getters.seasons as Season[]]
-    .filter(s => s.id !== seasonId);
-  context.commit('seasons', seasons);
-  if (context.getters.currentSeasonId === seasonId) {
-    context.commit('currentSeasonId', null);
-  }
+export default async function (context: ActionContext<SeasonModule, SeasonModule>, seasonId: string) {
+  const firebaseInstance = useFirebase();
+
+  await firebaseInstance.database()
+    .ref(`season/${seasonId}`)
+    .remove();
 }
