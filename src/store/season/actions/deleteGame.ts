fccf8c3175f9e1e5e3f91a6e8d3585c33281f663
Eder Benjamin <ben-eder@freenet.de>
2021-03-01 21:42:54 +0100

    feat: delete game at firebase

--- src/store/season/actions/deleteGame.ts
+++ src/store/season/actions/deleteGame.ts
@@ -1,13 +1,11 @@
 import { ActionContext } from 'vuex';
-import { DeleteSeasonGamePayload, Season, SeasonModule } from '../../../types';
+import { useFirebase } from '../../../firebase';
+import { DeleteSeasonGamePayload, SeasonModule } from '../../../types';
 
-export default function (context: ActionContext<SeasonModule, SeasonModule>, payload: DeleteSeasonGamePayload) {
-  const seasons = [...(context.getters.seasons as Season[])];
-  const season = seasons.find(s => s.id === payload.seasonId);
-  if (season) {
-    season.games = season.games.filter(g => g.id !== payload.gameId);
-    const seasonIndex = seasons.findIndex(s => s.id === season.id);
-    seasons[seasonIndex] = season;
-    context.commit('seasons', seasons);
-  }
+export default async function (context: ActionContext<SeasonModule, SeasonModule>, payload: DeleteSeasonGamePayload) {
+  const firebaseInstance = useFirebase();
+
+  await firebaseInstance.database()
+    .ref(`season/${payload.seasonId}/games/${payload.gameId}`)
+    .remove();
 }
