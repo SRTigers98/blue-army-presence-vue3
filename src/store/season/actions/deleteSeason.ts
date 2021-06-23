f389de512b63b814b15185a535f3d354fc2077dd
Eder Benjamin <ben-eder@freenet.de>
2021-06-23 15:38:35 +0200

    fix #2: Delete season action checks if the season is the current

--- src/store/season/actions/deleteSeason.ts
+++ src/store/season/actions/deleteSeason.ts
@@ -5,6 +5,11 @@ import { SeasonModule } from '../../../types';
 export default async function (context: ActionContext<SeasonModule, SeasonModule>, seasonId: string) {
   const firebaseInstance = useFirebase();
 
+  if (context.getters.currentSeasonId === seasonId) {
+    await firebaseInstance.database()
+      .ref('currentSeasonId')
+      .remove()
+  }
   await firebaseInstance.database()
     .ref(`season/${seasonId}`)
     .remove();
